e9138109758d135abdda5772820cef78e828fe48
Mike Thorn <mthorn@bahamut.(none)>
2013-04-10 13:27:43 -0400

    moving php into subdirectory

--- tests/run.php
+++ tests/run.php
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-include_once(__DIR__.'/../lib/dbm.php');
+include_once(__DIR__.'/../lib/php/dbm.php');
 
 global $output_path;
 $output_path = '';
